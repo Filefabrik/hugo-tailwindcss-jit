86a9d6d5333e79d6ae6fcba154ccb0056988db5f
FileFabrik <84433563+FileFabrik@users.noreply.github.com>
2021-05-26 12:23:57 +0200

    Update tailwind.config.js
    Split the Purge config
    https://tailwindcss.com/docs/optimizing-for-production#purge-css-options
    
    it is need if javascript generate class-names they are not trackable by tailwindcss

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-	// mode    : 'jit',
-	mode    : 'production' !== process.env.NODE_ENV ?'jit':'',
-	purge   : ['layouts/**/*.html'],
-	darkMode: false, // or 'media' or 'class'
-	theme   : {
-		extend: {},
-	},
-	variants: {},
-	plugins : [],
+  mode : 'production' !== process.env.NODE_ENV ? 'jit' : '',
+  purge: {
+    content: ['layouts/**/*.html'],
+    options: {
+      safelist: ['do_not_purge_classes_'],
+    },
+  },
+  
+  darkMode: false, // or 'media' or 'class'
+  theme   : {
+    extend: {},
+  },
+  variants: {},
+  plugins : [],
 }
