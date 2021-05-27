2f4b0e0c8b858edc42cebdb8a70403e97bf4b0da
FileFabrik <84433563+FileFabrik@users.noreply.github.com>
2021-05-27 14:25:37 +0200

    Update tailwind.config.js
    correcting the "mode" setting

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,16 +1,18 @@
-module.exports = {
-  mode : 'production' !== process.env.NODE_ENV ? 'jit' : '',
-  purge: {
-    content: ['layouts/**/*.html'],
-    options: {
-      safelist: ['do_not_purge_classes_'],
-    },
-  },
-  
-  darkMode: false, // or 'media' or 'class'
-  theme   : {
-    extend: {},
-  },
-  variants: {},
-  plugins : [],
-}
+const cfgTw = {
+	purge: {
+		content: ['layouts/**/*.html'],
+		options: {
+			safelist: ['do_not_purge_classes_'],
+		},
+	},
+	
+	darkMode: false, // or 'media' or 'class'
+	theme   : {
+		extend: {},
+	},
+	variants: {},
+	plugins : [],
+};
+'production' !== process.env.NODE_ENV ? cfgTw.mode = 'jit' : null;
+
+module.exports = cfgTw;
